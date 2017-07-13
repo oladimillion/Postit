cfcb83f56a181ecf1c9beb43437452cb49200856
Akande Oladimeji <akandeoladimeji9@hotmail.com>
2017-07-13 15:27:49 +0100

    installed dependencies for test

--- test/test.js
+++ test/test.js
@@ -1,41 +1,21 @@
-let chai = require('chai'),
-    chaiHttp = require('chai-http');
+const asserts = require('chai').assert;
+const request = require('supertest');
+const app = require('./server.js');
 
+describe('Signup tests', () => {
 
-chai.use(chaiHttp);
-
-let expect = chai.expect;
-
-describe("POST request ", () => {
-
-    let url = "127.0.0.1:3000";
-
-    it(" should return status 200", (done) => {
-
-        chai.request(url)
-            .get("/")
-            .end(function(err, res) {
-                expect(err).to.be.null;
-                expect(res).to.have.status(200);
-                done();
-            });
+  it('signup post url should be defined', (done) => {
+    request(app).post('/api/user/signup').send().end((err, res) => {
+      asserts.equal(res.statusCode, 200);
+      done();
     });
+  });
 
-    it(" should disallow unregister user", (done) => {
-        let user = {
-            username: "112paul",
-            password: "1234"
-        };
-        chai.request(url)
-            .post("/api/user/signin")
-            .send(user)
-            .end(function(err, res) {
-                expect(res.body.success).to.equal(false);
-                expect(res.body.message).to.equal("Username must " +
-				"start with character and must be alphabet or alphanumeric");
-                expect(err).to.be.null;
-                expect(res).to.have.status(200);
-                done();
-            });
+  it('should validate input parameters are  username,email and password', (done) => {
+    let data = {'username':'gbenga_ps','password':'some password','email':'ioyetade@gmail.com'};
+    request(app).post('/api/user/signup').send(data).end((err, res) => {
+      asserts.equal(res.body, true);
+      done();
     });
+  });
 });
