55c3f641631dc0e4e4cf2e93970e8eda80ea1dad
Akande Oladimeji <akandeoladimeji9@hotmail.com>
2017-07-26 14:06:32 -0700

    Change Sequelize to sequelize at import line

--- server/models/db.js
+++ server/models/db.js
@@ -1,51 +1,51 @@
-import Sequelize from "Sequelize";
+import Sequelize from "sequelize";
 
 import config from "../config/config.json";
 
 const env = config["development"];
 
 const sequelize = new Sequelize(env.database, env.username, env.password, {
-  host: env.host,
-  dialect: env.dialect,
-  logging: false
+	host: env.host,
+	dialect: env.dialect,
+	logging: false
 });
 
 const db = {
-  Users: sequelize.import("./users"),
-  Groups: sequelize.import("./groups"),
-  Messages: sequelize.import("./messages"),
-  UserGroups: sequelize.import("./usergroups"),
-  GroupMessages: sequelize.import("./groupmessages"),
+	Users: sequelize.import("./users"),
+	Groups: sequelize.import("./groups"),
+	Messages: sequelize.import("./messages"),
+	UserGroups: sequelize.import("./usergroups"),
+	GroupMessages: sequelize.import("./groupmessages"),
 };
 
 //Messages' one to one associate with Groups
 db.Messages.belongsTo(db.Groups, {
-  foreignKey: "groupId",
+	foreignKey: "groupId",
 })
 
 //UserGroups' one to one association with Groups
 db.UserGroups.belongsTo(db.Groups, {
-  foreignKey: "groupId",
+	foreignKey: "groupId",
 });
 
 //UserGroups' one to one association with Users
 db.UserGroups.belongsTo(db.Users, {
-  foreignKey: "userId",
+	foreignKey: "userId",
 });
 
 //GroupMessages' one to one association with Groups
 db.GroupMessages.belongsTo(db.Groups, {
-  foreignKey: "groupId",
+	foreignKey: "groupId",
 });
 
 //GroupMessages' one to one association with Users
 db.GroupMessages.belongsTo(db.Users, {
-  foreignKey: "userId",
+	foreignKey: "userId",
 });
 
 //GroupMessages' one to one association with Messages
 db.GroupMessages.belongsTo(db.Messages, {
-  foreignKey: "messageId",
+	foreignKey: "messageId",
 })
 
 db.sequelize = sequelize;
